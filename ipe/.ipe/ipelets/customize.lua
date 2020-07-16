0097682bbe82a55fd247cf271d52fb89f2fab27d
Philipp Glira <philipp.glira@ait.ac.at>
2020-07-16 20:11:27 +0200

    Initial config for ipe

---
+++ ipe/.ipe/ipelets/customize.lua
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------
+-- My customization ipelet: customize.lua
+----------------------------------------------------------------------
+-- Default preferences can be found in '/usr/share/ipe/7.2.13/lua/prefs.lua'
+-- Default shortcuts can be found in '/usr/share/ipe/7.2.13/lua/shortcuts.lua'
+
+-- External editor for editing text objects
+-- must contain '%s' for the temporary filename
+-- set this to nil to hide the "Editor" button on text dialogs
+prefs.external_editor = "vim --servername ipe --remote-wait %s"
+
+-- Should the external editor be called automatically?
+prefs.auto_external_editor = nil
+-- Should closing the external editor immediately close the editing dialog?
+prefs.editor_closes_dialog = nil
+
+-- Minimum size of dialog window for creating/editing text objects
+prefs.editor_size = { 600, 400 }
