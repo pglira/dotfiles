427f91e469ce924f75539284adb6493a9fa09d23
pglira <pipppoo@gmail.com>
2021-06-08 07:57:12 +0200

    Removed old nb, updated standard programs

--- install-all-dots.sh
+++ install-all-dots.sh
@@ -24,37 +24,13 @@ case "$(hostname)" in
         ./install-program-dots.sh passwords
         ./install-program-dots.sh readline
         ./install-program-dots.sh sxiv
-        ./install-program-dots.sh spacevim
+        ./install-program-dots.sh taskwarrior
         ./install-program-dots.sh tmux
         ./install-program-dots.sh texstudio
         ./install-program-dots.sh tigervnc
         ./install-program-dots.sh vifm
-        ./install-program-dots.sh xmodmap
-        ./install-program-dots.sh xresources
-        ;;
-    L1AS19 ) # old nb
-        ./install-program-dots.sh alacritty --merge-tags "font-N1AS21 own_colorscheme"
-        ./install-program-dots.sh appimages
-        ./install-program-dots.sh bash --merge-tags "L1AS19"
-        ./install-program-dots.sh dunst
-        ./install-program-dots.sh fonts
-        ./install-program-dots.sh i3 --merge-tags "work"
-        ./install-program-dots.sh i3blocks
-        ./install-program-dots.sh ipe
-        ./install-program-dots.sh keepmenu
-        ./install-program-dots.sh jetbrains
-        ./install-program-dots.sh latex
-        ./install-program-dots.sh matlab
-        ./install-program-dots.sh moc
-        ./install-program-dots.sh mpv
-        ./install-program-dots.sh newsboat
-        ./install-program-dots.sh passwords
-        ./install-program-dots.sh readline
-        ./install-program-dots.sh sxiv
-        ./install-program-dots.sh spacevim
-        ./install-program-dots.sh texstudio
-        ./install-program-dots.sh tmux
-        ./install-program-dots.sh vifm
+        ./install-program-dots.sh vim
+        ./install-program-dots.sh vimiv
         ./install-program-dots.sh xmodmap
         ./install-program-dots.sh xresources
         ;;
@@ -76,11 +52,14 @@ case "$(hostname)" in
         ./install-program-dots.sh newsboat
         ./install-program-dots.sh passwords
         ./install-program-dots.sh readline
+        ./install-program-dots.sh ssh
         ./install-program-dots.sh sxiv
-        ./install-program-dots.sh spacevim
+        ./install-program-dots.sh taskwarrior
         ./install-program-dots.sh texstudio
         ./install-program-dots.sh tmux
         ./install-program-dots.sh vifm
+        ./install-program-dots.sh vim
+        ./install-program-dots.sh vimiv
         ./install-program-dots.sh xmodmap
         ./install-program-dots.sh xresources
         ;;
